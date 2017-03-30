b4b85a19477f7932d1a388e207a862865d70b796
olga2288 <yelisolga@yandex.ru>
2017-03-30 17:34:46 +0300

    finished ex16

--- ex16_1.py
+++ ex16_1.py
@@ -1,29 +1,25 @@
 from sys import argv
 script, filename = argv
 
-print "We're going to erase %r" % filename
+print "We're going to read %r" % filename
 print "If you don't want that, hit CTRL-C (^C)"
 print "If you do want that, hit RETURN"
 raw_input("?")
 print "Opening the file..."
-target = open(filename, 'w')
+target = open(filename, 'r')
 
-print "Truncating the file. Goodbue!"
-target.truncate()
+print target.read()
 
 print "Now I'm going to ask you for three lines "
 
 line1 = raw_input("line 1: ")
-line2 = raw_input("line 2: ")
-line3 = raw_input("line 3: ")
+
 
 print "Now I'm going to write these to file"
 target.write(line1)
 target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
-
+print target.read()
 print "and finally, we close it"
+
+
 target.close()
