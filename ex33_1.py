30d77e71258fef539dd4d86f26c56fea548bfaa5
olga2288 <yelisolga@yandex.ru>
2017-04-17 14:09:04 +0300

    new files with loops

---
+++ ex33_1.py
@@ -0,0 +1,21 @@
+i = 0
+numbers = []
+
+while i < 6:
+    print "At the top i is %d" % i
+    numbers.append(i)
+
+    i = i + 1
+    print "Numbers now: ", numbers
+    print "At the bottom i is %d" % i
+
+print "The numbers: "
+for num in numbers:
+    print num,
+
+f = []
+
+for i in range(0, 6):
+        f.append(i)
+        i += 1
+print "\nf = ", f
